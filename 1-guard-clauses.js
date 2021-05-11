6ce2625229d68deb4fc13da42131602c6b1620cf
Ankit Tanna <ankittanna@hotmail.com>
2021-05-11 17:02:45 +0530

    Update 1-guard-clauses.js

--- 1-guard-clauses.js
+++ 1-guard-clauses.js
@@ -1,14 +1,18 @@
 function canDrink(person) {
-    if (person?.age === null) {
+    if (person?.age !== null) {
         if (person.age < 18) {
             console.log('Nope!');
+            return;
         } else if (person.age < 21) {
             console.log('Not in US');
+            return;
         } else {
-            console.log('Yes!')
+            console.log('Yes!');
+            return;
         }
     } else {
         console.log('You ar enot a person.');
+        return;
     }
 }
 
